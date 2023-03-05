de29bfca8a94c0816e5c886def97952eb20e6923
abeel50 <abeelmuhammad50@gmail.com>
2023-03-05 21:46:57 +0500

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,35 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+92-302-4181-704'>+92-302-4181-704</LinkItem>
+        </LinkColumn>
+
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:abeelmuhammad078@gmail.com'>abeelmuhammad078@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+
+        <SocialIcons href='https://github.com'>
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://linkedin.com'>
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://instagram.com'>
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
